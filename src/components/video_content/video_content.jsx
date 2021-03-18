7fad8d62bd8cafbe99c6ec631c3bd79d3ad1934c
sonseong10 <sonseong10@gmail.com>
2021-03-18 22:05:56 +0900

    Simplify video_content.jsx

--- src/components/video_content/video_content.jsx
+++ src/components/video_content/video_content.jsx
@@ -1,5 +1,4 @@
-import React, { memo } from 'react'
-import ContentInfo from './video_content_info'
+import React, { memo, useState } from 'react'
 import YoutubeIframe from './youtube_iframe'
 import './video_content.css'
 const decode = require('unescape')
@@ -7,15 +6,39 @@ const decode = require('unescape')
 const VideoContent = memo(
   ({ video, video: { snippet }, toggleLike, toggleLikeBtn }) => {
     const finishTitle = decode(snippet.title)
+    const [toggleMore, setToggleMore] = useState('hide')
+
+    const toggleMoreBtn = () =>
+      setToggleMore(toggleMore === 'hide' ? 'show' : 'hide')
+
     return (
       <section className="content">
         <YoutubeIframe videoTitle={finishTitle} videoId={video} />
-        <ContentInfo
-          video={video}
-          videoTitle={finishTitle}
-          toggleLike={toggleLike}
-          toggleLikeBtn={toggleLikeBtn}
-        />
+        <section className="content-info">
+          <h1 className="content-title">{finishTitle}</h1>
+          <p className="content-date">
+            {`Upload Date: ${snippet.publishedAt.substr(0, 10)}`}
+          </p>
+          <button className="like-btn" onClick={toggleLikeBtn}>
+            {toggleLike}
+          </button>
+        </section>
+        <section className="content-info">
+          <div className="channel-wrap">
+            <img
+              src={snippet.channels}
+              alt="channels img"
+              className="channel-logo"
+            />
+          </div>
+          <h3 className="content-channel">{snippet.channelTitle}</h3>
+          <button type="button" className="more-btn" onClick={toggleMoreBtn}>
+            더보기
+          </button>
+          <pre className={`content-desc ${toggleMore}`}>
+            {snippet.description}
+          </pre>
+        </section>
       </section>
     )
   }
