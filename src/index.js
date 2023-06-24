0a3b14b0070dbd719657bf12f6aea4393d06950c
sonseongyeol <sonseong10@gmail.com>
2023-06-24 12:49:49 +0900

    📦 패키지 업데이트

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
 import axios from 'axios'
 import YoutubeApi from './service/youtube_api'
+import { createRoot } from 'react-dom/client'
 
 const httpURL = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
 })
-
 const youtube = new YoutubeApi(httpURL)
-ReactDOM.render(
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
   <React.StrictMode>
     <App youtube={youtube} />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
