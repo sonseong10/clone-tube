73c8c0a0ef24589d428dd50ab2d8ab3fba11b21d
sonseongyeol <sonseong10@gmail.com>
2024-08-20 21:57:09 +0900

    next project initial commit

---
+++ next/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+
+const path = require("path");
+
+export default nextConfig;
